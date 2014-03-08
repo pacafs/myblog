60ac4cbf6aea3ad3303fb1668df8851002645355
Pedro Fernandes da Silva <paca.fs@gmail.com>
2014-03-08 02:54:43 +0000

    post&home controllers

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,27 @@
+class PostsController < ApplicationController
+
+	def index
+		@post_content1 = "Some text stored in a controller variable - {@post_content1}"
+		@post_content2 = "Some text stored in a controller variable - {@post_content2}"
+	end
+
+	def new
+	end
+
+	def create
+	end
+    
+    def edit
+	end
+
+	def update
+	end
+
+	def show
+	end
+
+	def destroy
+	end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Myblog::Application.routes.draw do
+  resources :posts
+
+  root "home#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
